67877021594afee4bf59c40513dde5e09d436648
Vincent Hardouin <vincenthardouin@icloud.com>
2021-08-01 14:54:53 +0200

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+function getGitHubActionsUrl(owner, repository) {
+  return `https://api.github.com/repos/${owner}/${repository}/dispatches`;
+}
+
+function getHeaders(authorization) {
+  return {
+    Authorization: authorization,
+  };
+}
+
+function getResponse(statusCode, body) {
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body,
+  };
+}
+
+exports.handler = async (event) => {
+  const { owner, repository } = event.queryStringParameters;
+  const url = getGitHubActionsUrl(owner, repository);
+  const headers = getHeaders(event.headers['Authorization']);
+  const data = { event_type: 'deploy' };
+
+  try {
+    const response = await axios.post(url, data, { headers });
+    return getResponse(200, response.data);
+  } catch (err) {
+    console.log(err);
+    return getResponse(500, 'Failed to call GitHub Actions');
+  }
+};
